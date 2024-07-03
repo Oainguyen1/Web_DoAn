38bbf7dd9d7064dee204618bc6ee692ff308928a
LENOVO\oain9 <oain9229@gmail.com>
2024-07-03 11:41:57 +0700

    no message

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,14 +1,28 @@
-const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config()
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3002
+const app = express();
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     return res.send('Hello');
-})
+});
 
-app.listen(port, () =>{
-    console.log('Sever is running in port: ', + port)
-})
+console.log('process.env.MONGO_DB', process.env.MONGO_DB);
+
+mongoose
+    .connect(
+        `mongodb+srv://oainguyen:${process.env.MONGO_DB}@cluster0.zduqar8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    )
+    .then(() => {
+        console.log('Connect DB success!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(port, () => {
+    console.log('Sever is running in port: ', +port);
+});
